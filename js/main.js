15f26a1fc9cf6fec2e656fec973bac9e5a654f22
lilirdz <liliana.rdz93@gmail.com>
2016-04-28 20:14:56 -0700

    updated html files

--- js/main.js
+++ js/main.js
@@ -4,32 +4,3 @@ for(var i=0;i<buttons.length;i++) {
   //or
   buttons[i].style["background-color"] = i % 2 === 0 ? "#FF6600" : "#696666";
 }
-
-function alternate(id){
-
-  if(document.getElementsByTagName){
-
-    var table = document.getElementById(id);
-
-    var rows = table.getElementsByTagName("tr");
-
-    for(i = 0; i < rows.length; i++){
-
-  //manipulate rows
-
-      if(i % 2 == 0){
-
-        rows[i].className = "even";
-        .even{background-color: gray;};
-
-      }else{
-
-        rows[i].className = "odd";
-
-      }
-
-    }
-
-  }
-
-}
