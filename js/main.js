073b39083b3176b497499ba9ed12c15d5b3f6982
lilirdz <liliana.rdz93@gmail.com>
2016-04-28 17:47:14 -0700

    added more html files

--- js/main.js
+++ js/main.js
@@ -5,3 +5,31 @@ for(var i=0;i<buttons.length;i++) {
   buttons[i].style["background-color"] = i % 2 === 0 ? "#FF6600" : "#696666";
 }
 
+function alternate(id){
+
+  if(document.getElementsByTagName){
+
+    var table = document.getElementById(id);
+
+    var rows = table.getElementsByTagName("tr");
+
+    for(i = 0; i < rows.length; i++){
+
+  //manipulate rows
+
+      if(i % 2 == 0){
+
+        rows[i].className = "even";
+        .even{background-color: gray;};
+
+      }else{
+
+        rows[i].className = "odd";
+
+      }
+
+    }
+
+  }
+
+}
